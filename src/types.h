43a5066adf0bd5443424ea0b43f019c781237ecc
Nathaniel Griswold <nategriswold@gmail.com>
2013-08-13 12:32:16 -0700

    Remove redef of some builtin types

--- src/types.h
+++ src/types.h
@@ -2,15 +2,13 @@
 #define __TYPES_H__
 
 #include <stdio.h>
+#include <stdint.h>
 
 typedef unsigned long long int u64_t;
 typedef unsigned char u8_t;
 typedef unsigned short u16_t;
 typedef unsigned char BOOL;
-typedef unsigned long long int uintptr_t;
 typedef unsigned long long int uintnative_t;
-
-typedef long int int64_t;
 
 #define NATIVE_WORD_SIZE (sizeof(uintnative_t))
 
